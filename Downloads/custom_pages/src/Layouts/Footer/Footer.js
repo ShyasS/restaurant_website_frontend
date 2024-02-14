0ae560107df4f19504562f7a571de8fe33f4f769
Shyas <Shyass1620@gmail.com>
2024-02-14 18:27:59 +0530

    Updated

--- Downloads/custom_pages/src/Layouts/Footer/Footer.js
+++ Downloads/custom_pages/src/Layouts/Footer/Footer.js
@@ -20,28 +20,22 @@ const Footer = () => {
           <Col md={1} xs={12} className="mt-md-5">
             <hr className='FooterHR1' />
           </Col>
-
-
           <Col md={3} xs={12}>
-            <Image src={require('../../assets/img/grandIndiaLogo1.png')} style={{ height: '70px', width: '190px',marginTop:'90px' }} className=' ms-5' />
+            <Image src={require('../../assets/img/grandIndiaLogo1.png')} style={{ height: '70px', width: '190px', marginTop: '90px' }} className=' ms-5' />
             <p style={{ margin: '10px 0' }} className=''>A distinctive, well-preserved and comfortable space, high-quality products, authentic cuisine, food and drinks are done flawlessly.</p>
-            <FontAwesomeIcon icon={faFacebook}  style={{ marginLeft: '90px' }}  />
-              <FontAwesomeIcon icon={faTwitter} style={{ marginLeft: '20px' }} />
-              <FontAwesomeIcon icon={faPinterest} style={{ marginLeft: '20px' }} />
-              <FontAwesomeIcon icon={faInstagram} style={{ marginLeft: '20px' }} />
+            <FontAwesomeIcon icon={faFacebook} style={{ marginLeft: '90px' }} />
+            <FontAwesomeIcon icon={faTwitter} style={{ marginLeft: '20px' }} />
+            <FontAwesomeIcon icon={faPinterest} style={{ marginLeft: '20px' }} />
+            <FontAwesomeIcon icon={faInstagram} style={{ marginLeft: '20px' }} />
           </Col >
-          <Col  md={1} xs={12} lassName="mt-md-5" >
-          <hr style={{ color: '#c6ac83', marginTop: '150px' }} />
+          <Col md={1} xs={12} lassName="mt-md-5" >
+            <hr style={{ color: '#c6ac83', marginTop: '150px' }} />
           </Col>
-    
           <Col md={3} xs={12}>
-
-<h3 className='mt-md-3' id='FooterH3'>OPENING TIME</h3>
-<p className='mt-md-4' id="FooterOpenPara">82 Place Charles de Gaulle, Paris</p>
-<p id="FooterOpenPara">+91 801-555-99-43</p>
-</Col>
-
-   
+            <h3 className='mt-md-3' id='FooterH3'>OPENING TIME</h3>
+            <p className='mt-md-4' id="FooterOpenPara">82 Place Charles de Gaulle, Paris</p>
+            <p id="FooterOpenPara">+91 801-555-99-43</p>
+          </Col>
         </Row>
       </Container>
 
